8bd0c1ccd8ab902082b3ef21210dd028e4949a30
Ignacio H. Gomez <ignaciohugog@gmail.com>
2016-07-29 12:44:55 -0300

    Initial Commit

---
+++ Catalog/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Catalog
+//
+//  Created by Ignacio H. Gomez on 7/29/16.
+//  Copyright © 2016 Ignacio H. Gomez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Catalog/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Catalog
+//
+//  Created by Ignacio H. Gomez on 7/29/16.
+//  Copyright © 2016 Ignacio H. Gomez. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
